4cdb5de639c754edfe605040bc001245899bf2bf
Kyle Meredith <Kyle@kyles-mbp.middlebury.edu>
2017-06-12 12:08:25 -0400

    Working serial transmission

--- Sources/SerialUtils/SerialUtils.swift
+++ Sources/SerialUtils/SerialUtils.swift
@@ -2,6 +2,7 @@
 //
 // Controls opening and closing of serial ports
 
+import Foundation
 import SwiftSerial
 
 public func openPort(portName: String) -> SerialPort {
@@ -14,7 +15,11 @@ public func openPort(portName: String) -> SerialPort {
         
         serialPort.setSettings(receiveRate: .baud9600,
                                transmitRate: .baud9600,
-                               minimumBytesToRead: 8)
+                               minimumBytesToRead: 1,
+                               timeout: 1,
+                               useHardwareFlowControl: true,
+                               useSoftwareFlowControl: true,
+                               processOutput: true)
     } catch {
         print("Error: \(error)")
     }
@@ -25,4 +30,30 @@ public func openPort(portName: String) -> SerialPort {
 public func closePort(serialPort: SerialPort) -> Void {
     serialPort.closePort()
     print("Port Closed")
+}
+
+
+extension String {
+    
+    /// Create `Data` from hexadecimal string representation
+    ///
+    /// This takes a hexadecimal representation and creates a `Data` object. Note, if the string has any spaces or non-hex characters (e.g. starts with '<' and with a '>'), those are ignored and only hex characters are processed.
+    ///
+    /// - returns: Data represented by this hexadecimal string.
+    
+    public func hexadecimal() -> Data? {
+        var data = Data(capacity: characters.count / 2)
+        
+        let regex = try! NSRegularExpression(pattern: "[0-9a-f]{1,2}", options: .caseInsensitive)
+        regex.enumerateMatches(in: self, range: NSMakeRange(0, utf16.count)) { match, flags, stop in
+            let byteString = (self as NSString).substring(with: match!.range)
+            var num = UInt8(byteString, radix: 16)!
+            data.append(&num, count: 1)
+        }
+        
+        guard data.count > 0 else { return nil }
+        
+        return data
+    }
+    
 }
--- Sources/SwitcherCtrl/main.swift
+++ Sources/SwitcherCtrl/main.swift
@@ -11,7 +11,6 @@ import SwiftSerial
 import SerialUtils
 
 var switcher: SerialPort?
-print("yo")
 
 func startSwitcher() -> Void {
     let portName: String = "/dev/cu.usbserial"
@@ -20,20 +19,22 @@ func startSwitcher() -> Void {
 
 func turnOn(portNum: Int) -> Void {
     do {
-        let command: String = "\(0x81808101 + (portNum << 16))"
-        var bytesWritten: Int        // # of bytes written by writeData
-        var commandReceived: String      // result of readLine
+        var commandInt: Int = 0x81808101 + (portNum << 16)
+        print(commandInt)
+        let commandBytes = NSData(bytes: &commandInt, length: 8)
+        var bytesWritten: Int               // # of bytes written by writeData
+        var commandReceived: Data      // result of readLine
         
-        print("Writing test num <\(command)> to serial port")
+        print("Writing test num <\(commandBytes)> to serial port")
         
-        bytesWritten = try switcher!.writeString(command)
+        bytesWritten = try switcher!.writeData(commandBytes as Data)
         
         print("Successfully wrote \(bytesWritten) bytes")
         print("Waiting to receive what was written...")
         
-        commandReceived = try switcher!.readString(ofLength: bytesWritten)
-        if command == commandReceived {
-            print("Received string is the same as transmitted string. Test successful!")
+        commandReceived = try switcher!.readData(ofLength: bytesWritten)
+        if commandBytes as Data == commandReceived {
+            print("Received command is the same as transmitted command. Test successful!")
         } else {
             print("Uh oh! Received string is not the same as what was transmitted. This was what we received,")
             print("<\(commandReceived)>")
@@ -43,8 +44,38 @@ func turnOn(portNum: Int) -> Void {
     }
 }
 
+func turnOnTest() -> Void {
+    do {
+        //var cmd: UInt = 0x81808101 + (3 << 16)
+        
+        var cmd: UInt32 = 0x81808101
+        let data = NSData(bytes: &cmd, length: 4)
+        
+        var bytesWritten: Int
+        var commandReceived: Data
+        
+        print("Writing <\(data)> to serial port")
+        
+        bytesWritten = try switcher!.writeData(data as Data)
+        commandReceived = try switcher!.readData(ofLength: bytesWritten)
+        
+        print("sent \(bytesWritten) of \(data)")
+//        print("received \(commandReceived)")
+        
+        
+        } catch {
+        print("Error: \(error)")
+    }
+}
+
+
+func closePort(port: SerialPort) -> Void {
+    // return true if CloseHandle returns nonzero
+    closePort(serialPort: port);
+}
+
 startSwitcher()
-turnOn(portNum: 1)
+turnOnTest()
 
 
 
